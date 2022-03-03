fc31d89701794efa22297a4e39314fc6f8b1a793
nicklausw <n@nicklausw.com>
2022-03-02 23:58:05 -0500

    no more testmath; update .gitignore

--- main.c
+++ main.c
@@ -26,22 +26,6 @@ void on_ready(struct discord *client)
   log_info("Logged in as %s!", bot->username);
 }
 
-void test_math(struct discord *client, const struct discord_message *msg) {
-  int precision = atoi(msg->content);
-  char numberstring[] = "9999999999999999999999999999999999999999999999999999999";
-  mpfr_t num, othernum;
-  mpfr_init2(num, precision);
-  mpfr_init2(othernum, precision);
-  mpfr_set_str(num, numberstring, 10, 0);
-  mpfr_set_str(othernum, numberstring, 10, 0);
-  for(int i = 0; i < 10; i++) {
-    mpfr_mul(num, num, othernum, 0);
-  }
-  mpfr_printf("PRECISION: %d\n\n%Rf\n\n",precision,num);
-  mpfr_clear(num);
-  mpfr_clear(othernum);
-}
-
 void do_math(struct discord *client, const struct discord_message *msg)
 {
   if (msg->author->bot) return;
@@ -72,9 +56,8 @@ int main(int argc, char **argv)
   struct discord *client = discord_init(argv[1]);
   discord_set_on_ready(client, &on_ready);
   discord_set_on_command(client, "domath", &do_math);
+  discord_set_on_command(client, "Domath", &do_math);
   discord_set_on_command(client, "DoMath", &do_math);
-  discord_set_on_command(client, "testmath", &test_math);
-  discord_set_on_command(client, "TestMath", &test_math);
   discord_run(client);
   return 0;
 }
