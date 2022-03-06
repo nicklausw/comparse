413c0b8e8ea015013e3f7761a3bbc61b7aa62891
nicklausw <n@nicklausw.com>
2022-03-06 13:26:39 -0500

    simplify main.c

--- main.c
+++ main.c
@@ -4,21 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 extern void *mathParse(char*);
-
-bool startsWith(char *a, char *b)
-{
-  for(int c = 0; c < strlen(a); c++) {
-    if(!isdigit(a[c])) {
-      a[c] = tolower(a[c]);
-    }
-  }
-  if(strncmp(a, b, strlen(b)) == 0) return 1;
-  return 0;
-}
 
 void on_ready(struct discord *client) 
 {
@@ -29,16 +16,15 @@ void on_ready(struct discord *client)
 void do_math(struct discord *client, const struct discord_message *msg)
 {
   if (msg->author->bot) return;
-  char *s = malloc(2000);
+  if(!strlen(msg->content)) return;
 
-  // make sure input has no garbage.
-  for(int c = 0; c < 2000; c++) {
-    s[c] = '\0';
-  }
+  char *s = calloc(2000, 1);
+
   strcpy(s, msg->content);
 
   // append a semicolon so you don't have to.
   strcat(s, ";");
+
   mathParse(s);
 
   struct discord_create_message params = { .content = s };
